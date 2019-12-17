764aa01278061a6c320b91ffc55a7912ed2df550
ant work mac <hoj.jun@gmail.com>
2019-12-17 13:10:37 -0800

    almost final

--- slacktest.js
+++ slacktest.js
@@ -3,29 +3,62 @@ const twit = require('twit');                        // twitter library from npm
 const fetch = require('node-fetch');                 // fetch to post to slack
 const config = require('./config');                  // config for auth
 const moment = require('moment');                    // moment library for dates
-
-
-
-const url = 'https://hooks.slack.com/services/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'
 const request = require('request')
-const slackMessage = 'hey'
+const url = config.slack_webhook
 
 
 
-request({
-    uri: url,
-    method: "POST",
-    json: {
-        "text": "hey"
-    }
-})
 
 
 
-//   request({
+// const data = {
+//     "text": "Lorem Ipsum",
+//   }
+
+const fetchTest = async() => {
+  await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({"text": "TESSF"})
+  });   
+}
+
+fetchTest()
+
+// fetch(url, {
+//       method: 'POST', // or 'PUT'
+//       body: JSON.stringify('text'), // data can be `string` or {object}!
+//     })
+
+
+
+
+
+
+// const fetchTest = async () => {
+//     const response = await fetch('https://dog.ceo/api/breeds/image/random');
+//     const data = await response.json()
+//     console.log(data)
+// }
+
+// fetchTest()
+
+
+
+
+// fetch(`https://dog.ceo/api/breeds/image/random`)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//     })
+//     .catch(error => console.error(error));
+
+
+
+// request({
 //     uri: url,
-//     method: 'POST',
+//     method: "POST",
 //     json: {
-//       "text": "hi"
+//         "text": "hey"
 //     }
-//   })
+// })
+
